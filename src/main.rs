503b543597982fafdca83b27a23cdcc0296761b5
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2020-04-20 10:56:10 +0300

    Minor formatting edit.

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,29 @@ use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() {
-    let args = CliArgs::from_args();
+	let args = CliArgs::from_args();
 
-    match args.path {
-        Some(path) => {
-            let file_content = file_reader::read_content(path).await;
-            let lex = Token::lexer(file_content.as_str());
+	if let Some(path) = args.path {
+		let file_content = file_reader::read_content(path).await;
+		let lex = Token::lexer(file_content.as_str());
 
-            for token in lex.spanned() {
-                println!("{:?}", token);
-            }
-        }
-        None => loop {
-            print!("> ");
-            let _ = io::stdout().flush();
+		for token in lex.spanned() {
+			println!("{:?}", token);
+		}
+	} else {
+		loop {
+			print!("> ");
+			let _ = io::stdout().flush();
 
-            let mut input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Unable to read user input");
+			let mut input = String::new();
+			io::stdin()
+				.read_line(&mut input)
+				.expect("Unable to read user input");
 
-            let lex = Token::lexer(input.as_str());
-            for token in lex.spanned() {
-                println!("{:?}", token);
-            }
-        },
-    }
+			let lex = Token::lexer(input.as_str());
+			for token in lex.spanned() {
+				println!("{:?}", token);
+			}
+		}
+	}
 }
