64406a383fdf243626c58ac5b71c899681d47889
Roee Shapira <roy.shap1@gmail.com>
2020-04-26 19:52:48 +0300

    Minor actions change.

--- src/lib/ast.rs
+++ src/lib/ast.rs
@@ -1,11 +1,10 @@
 struct Location {
-    pub line: u32,
-    pub column: u32,
+	pub line: u32,
+	pub column: u32,
 }
 pub struct Node {
-    pub node_type: String,
-    pub start: u32,
-    pub end: u32,
-    pub location: Location,
-    pub children: Vec<Node>,
+	pub node_type: String,
+	pub range: (usize, usize),
+	pub location: Location,
+	pub children: Vec<Node>,
 }
