978f6579ba7673ab539886c2086127737f4be6e3
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2020-05-03 11:58:02 +0300

    Updated integration tests.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,55 +1,53 @@
-use std::process::Output;
-
 mod common;
 
 #[cfg(test)]
-mod tests {
+mod integration_tests {
 	use super::*;
+	use exitcode;
+	use std::process::Output;
 
 	type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 	#[test]
 	fn sanity() -> TestResult {
 		let output: Output = common::cargo_run("-e 'console.log(123)'")?;
-		assert_eq!(output.status.code(), Some(0));
+		assert!(exitcode::is_success(output.status.code().unwrap()));
 
 		Ok(())
 	}
 
 	#[test]
-	#[ignore]
 	fn bad_local_file() -> TestResult {
-		let output: Output = common::cargo_run("--path ./no/such/file.ts")?;
-		assert_eq!(output.status.code(), Some(1));
+		let output: Output = common::cargo_run("--path './no/such/file.ts'")?;
+		assert!(exitcode::is_error(output.status.code().unwrap()));
 
 		Ok(())
 	}
 
 	#[test]
-	#[ignore]
 	fn bad_file_ext() -> TestResult {
-		let output = common::cargo_run(
-			"--path https://github.com/5c077m4n/http-responder/blob/master/src/index.txt",
+		let output: Output = common::cargo_run(
+			"--path 'https://github.com/5c077m4n/http-responder/blob/master/src/index.txt'",
 		)?;
-		assert_eq!(output.status.code(), Some(1));
+		assert!(exitcode::is_error(output.status.code().unwrap()));
 
 		Ok(())
 	}
 
 	#[test]
 	fn all_good_local_file() -> TestResult {
-		let output = common::cargo_run("--path ./assets/test-1.ts")?;
-		assert_eq!(output.status.code(), Some(0));
+		let output: Output = common::cargo_run("--path '../assets/test-1.ts'")?;
+		assert!(exitcode::is_success(output.status.code().unwrap()));
 
 		Ok(())
 	}
 
 	#[test]
 	fn all_good_remote_file() -> TestResult {
-		let output = common::cargo_run(
-			"--path https://github.com/5c077m4n/http-responder/blob/master/src/index.ts",
+		let output: Output = common::cargo_run(
+			"--path 'https://raw.githubusercontent.com/5c077m4n/http-responder/master/src/index.ts'",
 		)?;
-		assert_eq!(output.status.code(), Some(0));
+		assert!(exitcode::is_success(output.status.code().unwrap()));
 
 		Ok(())
 	}
